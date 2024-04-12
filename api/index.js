40d87405dd10dfe48bbc13a98703d0e84779bcd6
Lucas González <lucasgxnzalez@gmail.com>
2024-04-12 19:53:34 -0300

    modified api and vercel files............................

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 var cors = require('cors');
 const app = express();
@@ -8,6 +9,7 @@ app.get('/datos', (req, res) => res.send(json));
 app.use(express.static('public'));
 
 
+app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
 app.listen(3001, () => console.log('Server ready on port 3001.'));
 
 module.exports = app;
