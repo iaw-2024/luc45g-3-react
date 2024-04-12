670920800088f08b27e948c656e1a14ca2836068
Lucas González <lucasgxnzalez@gmail.com>
2024-04-12 19:58:14 -0300

    i hate vercel

--- api/index.js
+++ api/index.js
@@ -8,7 +8,6 @@ var json = require('./data.json');
 app.get('/datos', (req, res) => res.send(json));
 app.use(express.static('public'));
 
-
 app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
 app.listen(3001, () => console.log('Server ready on port 3001.'));
 
